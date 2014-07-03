887aacbef48fe9e5ff84a052679a57cddefb8eea
Choon-Chern Lim <limc@mayo.edu>
2014-07-03 14:21:53 -0500

    Added TODO list

---
+++ src/test/java/Todo.groovy
@@ -0,0 +1,17 @@
+class Todo {
+    // * old(...)
+    // where - array vs table
+    // @Shared
+    // @Unroll
+    // Mock
+    // Mock with >>> [] different response each time
+    // Mock _
+    // pojo.method(!null)
+    // dir.listFiles(_ as FileFilter)
+    // multiple then block
+    // interaction {}
+    // @Ignore
+    // @IgnoreRest
+    // @IgnoreIf({!jvm.java8})
+    // @Requires({jvm.java8})
+}
